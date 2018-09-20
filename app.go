9709bcebca90c7dfaf72ba26c89b3de9227134e0
Raphael Carvalho de Aguiar <carvalhodeaguiar123@gmail.com>
2018-09-20 15:13:04 -0300

    GO Pubsub implementation sample

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/option"
+)
+
+const iDProduto = "FornecidoPelaLifeApps"
+
+func subscribe(fila string, callBack func(msg *pubsub.Message, cancel context.CancelFunc)) error {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(
+		ctx,
+		"paje-datastore",
+		option.WithCredentialsFile("configPubSub.json"))
+	if err != nil {
+		panic(err)
+	}
+	sub := client.Subscription(fila)
+	cctx, cancel := context.WithCancel(ctx)
+	return sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		callBack(msg, cancel)
+	})
+}
+func main() {
+	err := subscribe(iDProduto+".remote", func(msg *pubsub.Message, cancel context.CancelFunc) {
+		defer msg.Ack()
+		fmt.Println("JSON do pedido: " + string(msg.Data))
+	})
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+# Documentação para configuração utilização da fila de pedidos paje-datastore
+## Versão: 1.3.0
+
+### Como configurar o pubsub no seu projeto: 
+1. Configurando acesso ao pubsub: https://cloud.google.com/docs/authentication/production#obtaining_and_providing_service_account_credentials_manually
+2. Como receber mensagens: https://cloud.google.com/pubsub/docs/quickstart-client-libraries#receive_messagesa
+
+	
+Dependências do exemplo em GO:
+   go get cloud.google.com/go/pubsub
